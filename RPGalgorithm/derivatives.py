f0e697e7acc62a43fa06648ae8f7006cca611f79
Olivier520100 <olivier520100@gmail.com>
2023-02-04 11:56:10 -0500

    Update derivatives.py

---
+++ RPGalgorithm/derivatives.py
@@ -0,0 +1,21 @@
+import requests
+import numpy as np
+
+## Wolfram query notation
+#
+# + in wolfram query is "%2B"
+# - in wolfram query is "-"
+# * in wolfram query is "*"
+# / in wolfrma query is "%2F"
+#
+##
+ 
+layerprobably = np.zeros(5)
+i = 1
+while i < 6:
+    layerprobably[i] 
+
+currentlayer = 0
+
+
+
