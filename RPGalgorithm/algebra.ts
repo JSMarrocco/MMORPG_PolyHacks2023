4b4f432e302719c99f7051e48131e3266e7f5790
Gabriel Sabérian <gabsab1000@gmail.com>
2023-02-04 23:51:56 -0800

    Added op rationel

--- RPGalgorithm/algebra.ts
+++ RPGalgorithm/algebra.ts
@@ -1,3 +1,4 @@
+//Lv 1
 function writeAlgebraEquation1(cst1: number, cst2: number, operation: number, xValue: number): string {
     let operationString = '';
     
@@ -23,13 +24,56 @@ function writeAlgebraEquation1(cst1: number, cst2: number, operation: number, xV
     return equation;
   }
   
-const cst1_1 = Math.floor(Math.random() * 31) - 15;
-const cst2_1 = Math.floor(Math.random() * 31) - 15;
+let cst1_1 = Math.floor(Math.random() * 31) - 15;
+let cst2_1 = Math.floor(Math.random() * 31) - 15;
 const operation_1 = Math.floor(Math.random() * 2) + 1;
 const xValue_1 = Math.floor(Math.random() * 31) - 15;
 
-
+while(cst1_1 === 0 && cst2_1 === 0){
+    cst1_1 = Math.floor(Math.random() * 21) - 10;
+    cst2_1 = Math.floor(Math.random() * 21) - 10;
+  }
+  
 console.log(writeAlgebraEquation1(cst1_1, cst2_1, operation_1, xValue_1));
+
+//Lv 2
+
+function writeAlgebraEquation3(cst1: number, cst2: number, cst3: number, xValue: number): string {
+    let equation = "";
+    let cst4 =  Math.round(cst1/(cst2 * (xValue - cst3)));
+
+    if (cst3 < 0) {
+        equation = cst1 + " " + "/" + " " + cst2 + "(" + "x" + " " + "+" + " " + (cst3 * -1) + ")" + " " + "=" + " " + cst4;
+    } else {
+        equation = cst1 + " " + "/" + " " + cst2 + "(" + "x" + " " + "-" + " " + cst3 + ")" + " " + "=" + " " + cst4;
+    }
+
+    return equation;
+}
+
+let cst1_3 = Math.floor(Math.random() * 21) - 10;
+let cst2_3 = Math.floor(Math.random() * 21) - 10;
+let cst3_3 = Math.floor(Math.random() * 21) - 10;
+let xValue_2 = Math.floor(Math.random() * 21) - 10;
+let denominator = cst2_3 * (xValue_2 - cst3_3);
+let cst4_3 = 0;
+let userInput_3 = 0;
+
+while (cst1_3 == 0 || denominator == 0 || cst4_3 % 1 !== 0 || cst4_3 == 0) {
+    cst1_3 = Math.floor(Math.random() * 21) - 10;
+    cst2_3 = Math.floor(Math.random() * 21) - 10;
+    cst3_3 = Math.floor(Math.random() * 21) - 10;
+    xValue_2 = Math.floor(Math.random() * 21) - 10;
+    denominator = cst2_3 * (xValue_2 - cst3_3);
+
+    if (denominator > 0 || denominator < 0) {
+        cst4_3 = cst1_3 / (cst2_3 * (xValue_2 - cst3_3));
+    }
+}
+
+console.log(writeAlgebraEquation3(cst1_3, cst2_3, cst3_3, xValue_2));
+
+//Lv 3
 
 function writeAlgebraEquation2(cst1: number, cst2: number, cst3: number): string {
     const cst2_2 = cst3 * cst1;
@@ -53,11 +97,18 @@ function writeAlgebraEquation2(cst1: number, cst2: number, cst3: number): string
   
 
   
-const cst1_2 = Math.floor(Math.random() * 21) - 10;
-const cst2_2 = Math.floor(Math.random() * 21) - 10;
-const cst3_2 = Math.floor(Math.random() * 21) - 10;
+let cst1_2 = Math.floor(Math.random() * 21) - 10;
+let cst2_2 = Math.floor(Math.random() * 21) - 10;
+let cst3_2 = Math.floor(Math.random() * 21) - 10;
 let equation_2 = "";
 let userInput_2 = "";
+
+while (cst1_2 == 0 || cst2_2 == 0 || cst3_2 == 0) {
+    cst1_2 = Math.floor(Math.random() * (10 - (-10) + 1) + (-10));
+    cst2_2 = Math.floor(Math.random() * (10 - (-10) + 1) + (-10));
+    cst3_2 = Math.floor(Math.random() * (10 - (-10) + 1) + (-10));
+  }
+  
 
 console.log(writeAlgebraEquation2(cst1_2, cst2_2, cst3_2));
 
